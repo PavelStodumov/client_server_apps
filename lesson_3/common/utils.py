4b2e73a0cee3282697972f2ee792286ef0e7ed49
Pavel <stodumoff@yandex.ru>
2022-05-24 14:22:09 +0300

    compleate

---
+++ lesson_3/common/utils.py
@@ -0,0 +1,32 @@
+import json
+from .variables import ENCODING, MAX_LENGHT_MESSAGE
+
+
+def send_message(socket, message):
+    '''
+    на вход передаётся сокет и сообщение в виде словаря
+    функция кодирует и отправляет сообщение
+    '''
+
+    if not isinstance(message, dict):
+        raise TypeError
+    message = json.dumps(message).encode(ENCODING)
+    socket.send(message)
+
+def get_message(client):
+    '''
+    функция принимает и декодирует сообщение
+    возвращает словарь
+    '''
+    encoded_response = client.recv(MAX_LENGHT_MESSAGE)
+    if isinstance(encoded_response, bytes):
+        json_response = encoded_response.decode(ENCODING)
+        if isinstance(json_response, str):
+            response = json.loads(json_response)
+            if isinstance(response, dict):
+                return response
+            raise ValueError
+        raise ValueError
+    raise ValueError
+
+
--- lesson_3/server.py
+++ lesson_3/server.py
@@ -1,14 +1,51 @@
+import sys
 from socket import *
+from urllib import response
+from common.variables import *
+from common.utils import *
 
-s = socket(AF_INET, SOCK_STREAM)
-s.bind(('', 8888))
-s.listen()
 
-# while True:
-for i in range(3):
-    client, addr = s.accept()
-    data = client.recv(1024).decode('utf-8')
-    print(data)
-    message = f'Ты написал {data}'
-    client.send(message.encode('utf-8'))
-    client.close()
+def response_message(message):
+    if message['action'] == PRESENCE and message['user']['account_name'] == 'guest':
+        return {
+            RESPONSE: '200',
+            'status': 'ok'
+            }
+    return {
+        RESPONSE: '400',
+        'status': 'Bad request'
+    }
+
+
+
+def main():
+    try:
+        port = int(sys.argv[sys.argv.index('-p') + 1]) if '-p' in sys.argv else DEFAULT_PORT
+        if not 1024 < port < 65535:
+            raise ValueError
+    except:
+        print('Номер порта должен быть числом в пределах от 1024 до 65535.')
+        sys.exit(1)
+    ip = sys.argv[sys.argv.index('-a') + 1] if '-a' in sys.argv else DEFAULT_IP
+
+    stream = socket(AF_INET, SOCK_STREAM)
+    stream.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    stream.bind((ip, port))
+    stream.listen(MAX_CONNECTIONS)
+
+
+
+    while True:
+        client, addr = stream.accept()
+        try:
+            message_from_client = get_message(client)
+            # print(message_from_client)
+            response = response_message(message_from_client)
+            send_message(client, response)
+            client.close()
+        except:
+            print('Принято некорректное сообщение от клиента')
+            client.close()
+
+if __name__ == '__main__':
+    main()
