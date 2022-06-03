3cf16a67e07eaa9e28d0a792066a9a5c82b3060e
Pavel <stodumoff@yandex.ru>
2022-06-03 16:16:13 +0300

    вроде работает

--- lesson_3/server.py
+++ lesson_3/server.py
@@ -1,4 +1,4 @@
-import sys, logging, logs.server_log_config
+import sys, logging, select
 from socket import *
 from common.variables import *
 from common.utils import *
@@ -18,6 +18,30 @@ def response_message(message):
         'status': 'Bad request'
     }
 
+def read_clients(r_clients, all_clients):
+    resp = {}
+    for sock in r_clients:
+        try:
+            data = sock.recv(1024).decode('utf-8')
+            resp[sock] = data
+        except:
+            print(f'Клиент {sock.fileno()} {sock.getpeername()} отключился')
+            all_clients.remove(sock)
+    return resp
+
+def write_clients(requests, w_clients, all_clients):
+    for sock in w_clients:
+        for value in requests.values():
+            sock.send(f'{value}'.encode(ENCODING))
+        
+        
+        # try:
+        #     resp = f'Клиент написали {requests[sock]}'.encode('utf-8')
+        #     sock.send(resp)
+        # except:
+        #     print(f'Клиент {sock.fileno()} {sock.getpeername()} отключился при ответе')
+        #     sock.close()
+        #     all_clients.remove(sock)
 
 
 def main():
@@ -28,26 +52,41 @@ def main():
     except:
         log.warning('Номер порта должен быть числом в пределах от 1024 до 65535.')
         sys.exit(1)
-    ip = sys.argv[sys.argv.index('-a') + 1] if '-a' in sys.argv else DEFAULT_IP
+    ip = sys.argv[sys.argv.index('-a') + 1] if '-a' in sys.argv else ''
 
     stream = socket(AF_INET, SOCK_STREAM)
     stream.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     stream.bind((ip, port))
     stream.listen(MAX_CONNECTIONS)
+    stream.settimeout(0.2)
     log.info(f'Запущен сервер: ip {ip}, port {port}')
 
-
-
+    clients = []
     while True:
-        client, addr = stream.accept()
         try:
-            message_from_client = get_message(client)
-            response = response_message(message_from_client)
-            send_message(client, response)
-            client.close()
-        except:
-            log.warning('Принято некорректное сообщение от клиента')
-            client.close()
+            client, addr = stream.accept()
+        except OSError:
+            pass
+        else:
+            log.info(f'Получен запрос на соединение от {addr}')
+            print(f'Получен запрос на соединение от {addr}')
+            clients.append(client)
+        finally:
+            r_clients = []
+            w_clients = []
+            try:
+                r_clients, w_clients, e = select.select(clients, clients, [])
+            except:
+                pass
+            requests = read_clients(r_clients, clients)
+            
+
+            if requests:
+                write_clients(requests, clients, clients)
+
+            if 'exit' in requests.values():
+                break
 
 if __name__ == '__main__':
+    print('Сервер запущен')
     main()
