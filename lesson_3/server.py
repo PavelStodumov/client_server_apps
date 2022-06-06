87abd31bcc6b9c97ea4e95fbedb46800ab01cb60
Pavel <stodumoff@yandex.ru>
2022-06-06 21:13:31 +0300

    working send message to client

--- lesson_3/server.py
+++ lesson_3/server.py
@@ -1,3 +1,5 @@
+from email import message
+from pydoc import cli
 import sys, logging, select
 from socket import *
 from common.variables import *
@@ -8,31 +10,40 @@ log = logging.getLogger('app.server')
 
 def response_message(message):
     log.info(f'Принято сообщение: {message}')
-    if message['action'] == PRESENCE and message['user']['account_name'] == 'guest':
+    if message[ACTION] == PRESENCE and message['user']['account_name'] == 'guest':
         return {
             RESPONSE: '200',
             'status': 'ok'
             }
+    elif message[ACTION] == 'message':
+        return message
+    elif message[ACTION] == 'intro':
+        return message
     return {
         RESPONSE: '400',
         'status': 'Bad request'
     }
 
 def read_clients(r_clients, all_clients):
+    # читаем сообщения от клиентов
     resp = {}
     for sock in r_clients:
         try:
-            data = sock.recv(1024).decode('utf-8')
-            resp[sock] = data
+            data = get_message(sock)
+            resp['from_user'] = data[USER][ACCOUNT_NAME]
+            resp['to_user'] = data['to_user']
+            resp['text'] = data['text']
         except:
             print(f'Клиент {sock.fileno()} {sock.getpeername()} отключился')
             all_clients.remove(sock)
     return resp
 
-def write_clients(requests, w_clients, all_clients):
-    for sock in w_clients:
-        for value in requests.values():
-            sock.send(f'{value}'.encode(ENCODING))
+def write_clients(requests, w_clients, all_clients, d_clients):
+    # отправляем сообщение пользователю
+    for c_socket, c_name in d_clients.items():
+        if requests['to_user'] == c_name and c_socket in w_clients:
+        
+            send_message(c_socket, requests)
 
 
 def main():
@@ -53,15 +64,19 @@ def main():
     log.info(f'Запущен сервер: ip {ip}, port {port}')
 
     clients = []
+    d_clients = {}
     while True:
         try:
             client, addr = stream.accept()
+            client_name = get_message(client)[USER][ACCOUNT_NAME]
         except OSError:
             pass
         else:
             log.info(f'Получен запрос на соединение от {addr}')
             print(f'Получен запрос на соединение от {addr}')
             clients.append(client)
+            d_clients[client] = client_name
+            print(d_clients)
         finally:
             r_clients = []
             w_clients = []
@@ -73,10 +88,10 @@ def main():
             
 
             if requests:
-                write_clients(requests, clients, clients)
+                write_clients(requests, clients, clients, d_clients)
 
-            if 'exit' in requests.values():
-                break
+            # if 'exit' in requests.values():
+            #     break
 
 if __name__ == '__main__':
     print('Сервер запущен')
---
+++ lesson_3/test.py
@@ -0,0 +1,7 @@
+d = {'dvs': 'svs', '3': 'egf'}
+
+print(d.keys())
+print(d.values())
+
+for i in d.keys():
+    print(i)
